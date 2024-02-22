487837a8d57c03eab0f13e5e24d35296f0dcd3b4
yukipeti <ugachan0817@gmail.com>
2024-02-23 00:08:05 +0900

    kotlin practice

--- untitled/src/main/kotlin/Main.kt
+++ untitled/src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
 fun main() {
-    println("Hello World!")
+    var num = "hello"
+    println("num = $num")
+    num = "nunti"
+    println("num = $num")
+
+    var sum = 0
+    for(i in 0 ..< 5){
+        sum += 1
+        println("sum = $sum")
+    }
+    println("sum = $sum")
+
+    val array = arrayOf("湊あくあ","number",1,"IDOL")
+    array.forEach { print(it) }
+    println()
+
+    val array2 = arrayOf("りんご","ごりら","らっぱ")
+    for(i in 0..2){
+        println(array2[i])
+    }
 }
